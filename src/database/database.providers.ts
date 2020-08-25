6eaa30d8aad8626af577f90b9b8c2462d2aabc68
andr11s <alvisandres1@gmail.com>
2020-08-25 18:14:35 -0500

    Funcionando

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -7,12 +7,12 @@ export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: async () => await createConnection({
-      type: 'mariadb',
-      host: 'localhost',
+      type: 'mysql',
+      host: 'mysql5019.site4now.net',
       port: 3306,
-      username: 'root',
-      password: '',
-      database: 'YucappDB',
+      username: '9f3d39_yucapp',
+      password: 'Yuc4App.2020',
+      database: 'db_9f3d39_yucapp',
       entities: [
         'dist/models/*.entity{.ts,.js}',
       ],
